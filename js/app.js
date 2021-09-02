5b1e98c0e3ef85495451dbadd1cddebffdb8c209
MusfiqueHasan <musfiquehasan08@gmail.com>
2021-09-02 22:11:29 +0600

    project done

--- js/app.js
+++ js/app.js
@@ -3,17 +3,20 @@ const searchBook = async () => {
     searchText = searchField.value;
     searchField.value = '';
 
-    // Error handling on load data
+    document.getElementById('error').textContent = '';
+
+    // Error handling on loaded data
     if (searchText === '') {
-        displayMessage('error', 'text-danger', 'hiii');
-       
+        displayMessage('error','text-danger','If you want to get your desire books? write the name of it on search box...');
+        document.getElementById('total-result').textContent = '';
+        document.getElementById('search-result').textContent = '';
+
     } else {
         toggleSpinner('visible');
         toggleDisplayResult('hidden');
         const url = `https://openlibrary.org/search.json?q=${searchText}`;
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data.docs)
         displayBookResult(data.docs);
 
     }
@@ -30,45 +33,41 @@ const toggleDisplayResult = (resultValue) => {
     document.getElementById('search-result').style.visibility = resultValue;
 }
 
-const error = (errorValue) => {
-    document.getElementById('error').style.display = errorValue;
-
-}
-
-// displaying total result & error message function
+// displaying error and total result functionality
 const displayMessage = (id, style, value) => {
     const totalSearchResult = document.getElementById(id);
     totalSearchResult.textContent = '';
     const resultDiv = document.createElement('div');
     resultDiv.innerHTML = `<h5 class=" text-center ${style} fw-bold mb-3 " >${value} `;
     totalSearchResult.appendChild(resultDiv);
-    return totalSearchResult;
-
 }
 
 // display function
 const displayBookResult = (bookData) => {
+    // displaying total result
+    const totalResultMessage = `Total ${searchText.toUpperCase()} book found ${bookData.length}...</h5>`;
+    displayMessage('total-result', 'text-success', totalResultMessage);
+
 
     // Error handling on searched books
     if (bookData.length === 0) {
-        displayMessage('error', 'text-danger', `Book does not exist,, please try again`);
-        // toggleDisplayResult('hidden');
-    } else {
-        // displaying total result
-        const totalValue = `Total ${searchText.toUpperCase()} Book found ${bookData.length}...</h5>`;
-        displayMessage('total-result', 'text-success', totalValue);        
-        error('none');
-        // to find the searched books
-        const searchResult = document.getElementById('search-result');
-        searchResult.textContent = '';
-        bookData.slice(0, 21).forEach((book) => {
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+         displayMessage('error','text-danger','Books are not exist,,,please try again');
+        document.getElementById('total-result').textContent = '';
+        
+    }
+
+
+    // to find the searched books
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    bookData.slice(0, 21).forEach((book) => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
         <div class="card mb-3 h-100 p-3 shadow" style="max-width: 450px;">
             <div class="row g-0">
                <div class="col-md-4 ">
-               <img src=" https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : -1}-M.jpg" class="img-fluid rounded-start" alt="image not found">
+               <img src=" https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded-start" alt="image not found">
                </div>
                <div class="col-md-8">
                   <div class="card-body">
@@ -81,9 +80,9 @@ const displayBookResult = (bookData) => {
             </div>
         </div>
         `;
-            searchResult.appendChild(div);
-        });
-    }
-    toggleSpinner('hidden')
+        searchResult.appendChild(div);
+    });
+
+    toggleSpinner('hidden');
     toggleDisplayResult('visible');
 }
